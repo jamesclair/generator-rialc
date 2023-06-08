b4d2549c5234ec4cb375dff992b2c8ce7fe75046
jamesclair <clair.james88@gmail.com>
2023-06-07 21:36:39 -0500

    feat: add add more routes and index styling

--- generators/frontend/app/root.tsx
+++ generators/frontend/app/root.tsx
@@ -1,15 +1,14 @@
-import { LiveReload, Outlet } from "@remix-run/react";
+import { LiveReload, Outlet, Links } from "@remix-run/react";
+import React from "react";
 
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1"
-        />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <title>Remix: So great, it's funny!</title>
+        <Links />
       </head>
       <body>
         <Outlet />
